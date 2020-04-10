8d51e83139b83a46c775bd3955fd082cff13e427
bproorda <bproorda@gmail.com>
2020-04-10 11:13:11 -0500

    modularized hiking! still not broken!

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const superagent = require('superagent');
 const pg = require('pg');
 const locationHandler = require('./modules/location');
 const weatherHandler = require('./modules/weather');
+const trailHandler = require('./modules/hiking');
 const client = require('./util/db');
 
 
@@ -37,33 +38,6 @@ app.get('/bad', (request, response) => {
 //route for hiking trails
 app.get('/trails', trailHandler);
 
-function trailHandler(request, response) {
-  const lat = request.query.latitude;
-  const lon = request.query.longitude;
-  const url = 'https://www.hikingproject.com/data/get-trails';
-  superagent(url)
-    .query({
-      key: process.env.HIKING_KEY,
-      lat: lat,
-      lon: lon,
-      format: 'json'
-
-    })
-    .then(trailsResponse => {
-      let trailData = trailsResponse.body.trails;
-      console.log(trailData);
-      let trails = trailData.map(data => {
-        return new Trail(data);
-      })
-
-      response.send(trails);
-    })
-    .catch(err => {
-      console.log(err);
-      errorHandler(err, request, response);
-    });
-}
-
 //has to happen after error has occured
 app.use(errorHandler); // Error Middleware
 app.use(notFoundHandler);
@@ -81,26 +55,6 @@ function notFoundHandler(request, response) {
   });
 }
 
-function Location(city, geoData) {
-  this.search_Query = city;
-  this.formatted_query = geoData[0].display_name;
-  this.latitude = parseFloat(geoData[0].lat);
-  this.longitude = parseFloat(geoData[0].lon);
-
-}
-
-function Trail(trailData) {
-  this.name = trailData.name;
-  this.location = trailData.location;
-  this.length = trailData.length;
-  this.stars = trailData.stars;
-  this.star_votes = trailData.starVotes;
-  this.summary = trailData.summary;
-  this.trail_url = trailData.trail_url;
-  this.conditions = trailData.conditions;
-  this.condition_date = new Date(trailData.conditionDate).toDateString();
-  this.condition_time = new Date(trailData.conditionDate).toLocaleTimeString();
-}
 // Make sure the server is listening for requests after connecting to Database first
 client.connect()
   .then(() => {
